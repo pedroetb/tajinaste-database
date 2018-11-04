510f24bb5eece1e0c05ca38ac7ed02920751b480
Pedro Trujillo <pedroetb@gmail.com>
2018-11-04 16:39:20 +0000

    Allow users to read 'regular' people field

--- sql/roles.sql
+++ sql/roles.sql
@@ -21,7 +21,7 @@ END
 $$;
 GRANT tajinaste_user TO postgres;
 GRANT USAGE ON SCHEMA api TO tajinaste_user;
-GRANT SELECT(id, uuid, name, surname, entry, federated, photo) ON api.people TO tajinaste_user;
+GRANT SELECT(id, uuid, name, surname, entry, regular, federated, photo) ON api.people TO tajinaste_user;
 
 DO $$
 BEGIN
